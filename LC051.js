538dfb7871b4e99ba78e2d68c6feaa48e77f64ef
lalawow <lalawow@gmail.com>
2017-12-03 16:44:15 +0800

    LC051 N-Queens

---
+++ LC051.js
@@ -0,0 +1,63 @@
+/** https://leetcode.com/problems/n-queens/description/
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function(n) {
+    if (n == 1) return [
+        ["Q"]
+    ]
+    if (n < 4) return []
+    let res = []
+
+
+    var getResult = (pos) => {
+        if (pos.length == n) {
+            res.push(pos)
+            return
+        }
+        for (let i = 0; i < n; i++) {
+            if (check(pos, i)) {
+                getResult(pos + i)
+            }
+        }
+        if (pos == "") {
+            return displayRes()
+        }
+    }
+
+    var displayRes = () => {
+        let display = []
+        let mode = ""
+        for (let i = 0; i < n; i++) {
+            mode += "."
+        }
+        res.forEach((pos) => {
+            let oneResult = []
+                //console.log(pos)
+            for (let i = 0; i < n; i++) {
+                let str = mode
+                let p = parseInt(pos[i])
+                oneResult.push(str.substr(0, p) + "Q" + str.substr(p + 1, n))
+                    //console.log(parseInt(pos[i]))
+            }
+            display.push(oneResult)
+        })
+        return display
+    }
+
+    var check = function(str, newNum) {
+        let l = str.length
+        if (l == 0) return true
+        for (let i = 0; i < l; i++) {
+            let strNum = parseInt(str[i])
+            if (strNum == newNum) return false
+            if ((l - i) == (newNum - strNum) || (l - i) == (strNum - newNum)) return false
+        }
+        return true
+    }
+
+    return getResult("")
+};
+
+console.log(solveNQueens(5))
+    //console.log(solveNQueens(8))
